32105f1597bd22e44e4430f2613a9dba87678556
seanlo <seanlo@mit.edu>
2022-10-19 00:59:12 -0400

    revamp; test for arc preprocessing, benchmarking

--- test_electric_trucking.jl
+++ test_electric_trucking.jl
@@ -17,9 +17,113 @@ nocharge_data, charge_data = generate_instance_pair(
 plot_instance(nocharge_data, false)
 plot_instance(charge_data, true)
 
+# Results: without preprocessing arcs
+arc_nocharge_results, arc_nocharge_params = arc_formulation(
+    nocharge_data, 
+    false,
+);
+arc_charge_results, arc_charge_params = arc_formulation(
+    charge_data, 
+    true, 
+    time_limit = 3600
+);
 
-arc_nocharge_results, arc_nocharge_params = arc_formulation(nocharge_data, false);
-results_printout(arc_nocharge_results, arc_nocharge_params, nocharge_data, false)
+paths = construct_paths(arc_nocharge_results, nocharge_data)
+arc_chargesep_results, arc_chargesep_params = arc_formulation(
+    charge_data, 
+    true, 
+    true; 
+    paths = paths, 
+    time_limit = 3600
+);
 
-arc_charge_results, arc_charge_params = arc_formulation(charge_data, true, time_limit = 900);
-results_printout(arc_charge_results, arc_charge_params, charge_data, true)
+# Results: with preprocessing arcs
+nocharge_pp_data = preprocess_arcs(nocharge_data, false)
+arc_nocharge_pp_results, arc_nocharge_pp_params = arc_formulation(
+    nocharge_pp_data, 
+    false,
+);
+paths_pp = construct_paths(arc_nocharge_pp_results, nocharge_pp_data)
+
+arc_nocharge_results["objective"] ≈ arc_nocharge_pp_results["objective"]
+issetequal(values(paths), values(paths_pp))
+
+charge_pp_data = preprocess_arcs(charge_data, true, true)
+arc_charge_pp_results, arc_charge_pp_params = arc_formulation(
+    charge_pp_data, 
+    true, 
+    time_limit = 3600
+);
+arc_chargesep_pp_results, arc_chargesep_pp_params = arc_formulation(
+    charge_pp_data, 
+    true, 
+    true; 
+    paths = paths_pp, 
+    time_limit = 3600
+);
+
+# Stricter version of arc preprocessing
+charge_pp_e_data = preprocess_arcs(charge_data, true, false)
+arc_charge_pp_e_results, arc_charge_pp_e_params = arc_formulation(
+    charge_pp_e_data, 
+    true, 
+    time_limit = 3600
+);
+arc_chargesep_pp_e_results, arc_chargesep_pp_e_params = arc_formulation(
+    charge_pp_e_data, 
+    true, 
+    true; 
+    paths = paths_pp, 
+    time_limit = 3600
+);
+
+# Printouts
+results_printout(
+    arc_nocharge_results, arc_nocharge_params, 
+    nocharge_data, 
+    false,
+)
+results_printout(
+    arc_nocharge_pp_results, 
+    arc_nocharge_pp_params, 
+    nocharge_pp_data, 
+    false,
+)
+
+results_printout(
+    arc_charge_results, 
+    arc_charge_params, 
+    charge_data, 
+    true,
+)
+results_printout(
+    arc_charge_pp_results, 
+    arc_charge_pp_params, 
+    charge_pp_data, 
+    true,
+)
+results_printout(
+    arc_charge_pp_e_results, 
+    arc_charge_pp_e_params, 
+    charge_pp_e_data, 
+    true,
+)
+
+results_printout(
+    arc_chargesep_results, 
+    arc_chargesep_params, 
+    charge_data, 
+    true,
+)
+results_printout(
+    arc_chargesep_pp_results, 
+    arc_chargesep_pp_params, 
+    charge_pp_data, 
+    true,
+)
+results_printout(
+    arc_chargesep_pp_e_results, 
+    arc_chargesep_pp_e_params, 
+    charge_pp_e_data, 
+    true,
+)
