9d8cf4b16f11fefb855e0c5c155bc0bb7ef7e13e
seanlo <seanlo@mit.edu>
2023-06-06 19:35:24 -0400

    implement baseline (first commit)

--- src/subpath_formulation.jl
+++ src/subpath_formulation.jl
@@ -32,6 +32,32 @@ Base.copy(p::PathLabel) = PathLabel(
     copy(p.served),
 )
 
+mutable struct FullPathLabel
+    cost::Float64
+    nodes::Vector{Int}
+    excesses::Vector{Int}
+    slacks::Vector{Int}
+    time_mincharge::Int
+    time_maxcharge::Int
+    charge_mincharge::Int
+    charge_maxcharge::Int
+    explored::Bool
+    served::Vector{Int}
+end
+
+Base.copy(p::FullPathLabel) = FullPathLabel(
+    p.cost,
+    copy(p.nodes),
+    copy(p.excesses),
+    copy(p.slacks),
+    p.time_mincharge,
+    p.time_maxcharge,
+    p.charge_mincharge,
+    p.charge_maxcharge,
+    p.explored,
+    copy(p.served),
+)
+
 function generate_artificial_subpaths(data)
     artificial_subpaths = Dict{
         Tuple{Tuple{Int, Int, Int}, Tuple{Int, Int, Int}},
@@ -712,10 +738,372 @@ function get_subpaths_charging_arcs_from_negative_paths(
     return generated_subpaths, generated_charging_arcs
 end
 
+function find_nondominated_paths(
+    G,
+    data, 
+    κ,
+    μ,
+    ν,
+    ;
+    single_service::Bool = false,
+    time_windows::Bool = true,
+    check_customers::Bool = false,
+)
+    function add_full_path_label_to_collection!(
+        collection::Union{
+            SortedDict{
+                Tuple{Int, Int, Int}, 
+                FullPathLabel, 
+            },
+            SortedDict{
+                Tuple{Int, Int, Int, Vararg{Int}}, 
+                FullPathLabel, 
+            },
+        },
+        key::Union{
+            Tuple{Int, Int, Int},
+            Tuple{Int, Int, Int, Vararg{Int}},
+        },
+        path::FullPathLabel,
+        ;
+        verbose::Bool = false,
+    )
+        added = true
+        for (k, p) in pairs(collection)
+            if p.cost ≤ path.cost
+                if all(k .≤ key)
+                    added = false
+                    if verbose
+                        println("$(key), $(path.cost) dominated by $(k), $(p.cost)")
+                    end
+                    break
+                end
+            end
+            if path.cost ≤ p.cost
+                if all(key .≤ k)
+                    if verbose
+                        println("$(key), $(path.cost) dominates $(k), $(p.cost)")
+                    end
+                    pop!(collection, k)
+                end
+            end
+        end
+        if added
+            if verbose
+                println("$(key), $(path.cost) added!")
+            end
+            insert!(collection, key, path)
+        end
+        return added
+    end
+
+    modified_costs = data["travel_cost_coeff"] * Float64.(copy(data["c"]))
+    for j in data["N_customers"]
+        for i in data["N_nodes"]
+            modified_costs[i,j] -= ν[j]
+        end
+    end
+
+    if check_customers
+        full_labels = Dict(
+            starting_node => Dict(
+                current_node => SortedDict{
+                    Tuple{Int, Int, Int, Vararg{Int}}, 
+                    FullPathLabel
+                }()
+                for current_node in data["N_nodes"]
+            )
+            for starting_node in data["N_depots"]
+        )
+    else
+        full_labels = Dict(
+            starting_node => Dict(
+                current_node => SortedDict{
+                    Tuple{Int, Int, Int}, 
+                    FullPathLabel
+                }()
+                for current_node in data["N_nodes"]
+            )
+            for starting_node in data["N_depots"]
+        )
+    end
+
+    if check_customers
+        # label key here has the following fields:
+        # 1) current minimum time T_i(min)
+        # 2) negative of current max charge -B_i(max)
+        # 3) difference between min time and min charge, T_i(min) - B_i(min)
+        # 4) if applicable, whether i-th customer served
+        key = (0, -data["B"], -data["B"], zeros(Int, data["n_customers"])...)
+    else
+        key = (0, -data["B"], -data["B"])
+    end
+    for depot in data["N_depots"]
+        full_labels[depot][depot][key] = FullPathLabel(
+            0.0,
+            [depot],
+            Int[],
+            Int[],
+            0,
+            0,
+            data["B"],
+            data["B"],
+            false,
+            zeros(Int, data["n_customers"]),
+        )
+    end
+    unexplored_states = SortedSet(
+        [
+            (key..., depot, depot)
+            for depot in data["N_depots"]
+        ]
+    )
+
+    t = data["t"]
+    B = data["B"]
+    q = data["q"]
+    if time_windows
+        α = data["α"]
+        β = data["β"]
+    else
+        α = zeros(Int, data["n_nodes"])
+        β = repeat([data["T"]], data["n_nodes"])
+    end
+
+    while length(unexplored_states) > 0
+        # println(length(unexplored_states))
+        state = pop!(unexplored_states)
+        starting_node = state[end-1]
+        i = state[end]
+        if !(state[1:end-2] in keys(full_labels[starting_node][i]))
+            continue
+        end
+        path = full_labels[starting_node][i][state[1:end-2]]
+        # println("$(path.time_mincharge), $(path.time_maxcharge), $(path.charge_mincharge), $(path.charge_maxcharge)")
+        for j in setdiff(outneighbors(G, i), i)
+            # println("$state -> $j")
+            if j in data["N_customers"] && single_service && path.served[j] > 0
+                # println("already served $j")
+                continue
+            end
+            # feasibility checks
+            # (1) battery
+            excess = max(
+                0, 
+                q[i,j] - path.charge_mincharge 
+            )
+            # (2) time windows
+            if path.time_mincharge + excess + t[i,j] > β[j]
+                # println("$(path.time_mincharge), $excess, $(t[i,j]), $(β[j])")
+                # println("not time windows feasible")
+                continue
+            end
+            # (3) charge interval 
+            if (
+                (i in data["N_charging"] && excess > max(B - path.charge_mincharge, 0))
+                || 
+                (!(i in data["N_charging"]) && excess > max(path.charge_maxcharge - path.charge_mincharge, 0))
+            )
+                # if i in data["N_charging"]
+                #     println("$excess, $(B), $(path.charge_mincharge)")
+                # else
+                #     println("$excess, $(path.charge_maxcharge), $(path.charge_mincharge)")
+                # end
+                # println("not charge feasible")
+                continue
+            end
+            
+            new_path = copy(path)
+            push!(new_path.nodes, j)
+            if j in data["N_customers"]
+                new_path.served[j] += 1
+            end
+
+            push!(new_path.excesses, excess)
+            new_path.time_mincharge = max(
+                α[j],
+                path.time_mincharge + t[i,j] + excess
+            )
+            if i in data["N_charging"]
+                slack = max(
+                    # floating point accuracy
+                    0, 
+                    min(
+                        new_path.time_mincharge - (path.time_mincharge + t[i,j] + excess),
+                        B - (path.charge_mincharge + excess),
+                    )
+                )
+                push!(new_path.slacks, slack)
+                new_path.time_maxcharge = min(
+                    β[j],
+                    max(
+                        α[j],
+                        path.time_mincharge + (B - path.charge_mincharge) + t[i,j],
+                    )
+                )
+            else
+                slack = max(
+                    # floating point accuracy
+                    0, 
+                    min(
+                        new_path.time_mincharge - (path.time_mincharge + t[i,j] + excess),
+                        path.charge_maxcharge - (path.charge_mincharge + excess),
+                    )
+                )
+                push!(new_path.slacks, slack)
+                new_path.time_maxcharge = min(
+                    β[j],
+                    max(
+                        α[j],
+                        path.time_maxcharge + t[i,j],
+                    )
+                )
+            end
+            
+            new_path.charge_mincharge = (
+                path.charge_mincharge 
+                + excess 
+                + slack
+                - q[i,j]
+            )
+            new_path.charge_maxcharge = (
+                new_path.charge_mincharge 
+                + new_path.time_maxcharge 
+                - new_path.time_mincharge
+            )
+
+            new_path.cost += modified_costs[i,j]
+            new_path.cost += data["charge_cost_coeff"] * (slack + excess)
+            
+            # println("$(new_path.time_mincharge), $(new_path.time_maxcharge), $(new_path.charge_mincharge), $(new_path.charge_maxcharge)")
+            # add new_path to collection
+            if check_customers
+                new_key = (
+                    new_path.time_mincharge, 
+                    - new_path.charge_maxcharge, 
+                    new_path.time_mincharge - new_path.charge_mincharge, 
+                    new_path.served...,
+                )
+            else
+                new_key = (
+                    new_path.time_mincharge, 
+                    - new_path.charge_maxcharge, 
+                    new_path.time_mincharge - new_path.charge_mincharge
+                )
+            end
+
+            added = add_full_path_label_to_collection!(full_labels[starting_node][j], new_key, new_path, verbose = false)
+            if added && !(j in data["N_depots"])
+                new_state = (new_key..., starting_node, j)
+                if !(new_state in unexplored_states)
+                    # println("adding state: $(new_state)")
+                    push!(unexplored_states, new_state)
+                end
+            end
+        end
+    end
+    
+    for starting_node in data["N_depots"]
+        for ending_node in union(data["N_customers"], data["N_charging"])
+            delete!(full_labels[starting_node], ending_node)
+        end
+    end
+    for starting_node in data["N_depots"]
+        for ending_node in  data["N_depots"]
+            for path in values(full_labels[starting_node][ending_node])
+                path.cost = path.cost - κ[starting_node] - μ[ending_node]
+            end
+        end
+    end
+
+    return full_labels
+end
+
+function get_subpaths_charging_arcs_from_negative_path_labels(
+    data, 
+    full_labels,
+)
+    generated_subpaths = Dict{
+        Tuple{
+            Tuple{Int, Int, Int}, 
+            Tuple{Int, Int, Int}
+        }, 
+        Vector{Subpath},
+    }()
+    generated_charging_arcs = Dict{
+        Tuple{
+            Tuple{Int, Int, Int}, 
+            Tuple{Int, Int, Int}
+        }, 
+        Vector{ChargingArc},
+    }()
+    for starting_node in data["N_depots"], end_node in data["N_depots"]
+        for path in values(full_labels[starting_node][end_node])
+            if path.cost ≥ -1e-6
+                continue
+            end
+            states = Tuple{Int, Int, Int}[]
+            current_subpath = Subpath(
+                n_customers = data["n_customers"],
+                starting_node = path.nodes[1],
+                starting_time = 0.0, 
+                starting_charge = data["B"],
+            )
+            i = path.nodes[1]
+            for (j, e, s) in zip(path.nodes[2:end], path.excesses, path.slacks)
+                current_subpath.current_node = j
+                push!(current_subpath.arcs, (i, j))
+                current_subpath.starting_time += (e + s)
+                current_subpath.starting_charge += (e + s) 
+                current_subpath.current_time += (data["t"][i,j] + e + s)
+                current_subpath.current_charge += (- data["q"][i,j] + e + s)
+                if j in data["N_charging"]
+                    push!(
+                        states, 
+                        (current_subpath.starting_node, current_subpath.starting_time, current_subpath.starting_charge), 
+                        (current_subpath.current_node, current_subpath.current_time, current_subpath.current_charge), 
+                    )
+                    add_subpath_to_generated_subpaths!(generated_subpaths, current_subpath)
+                    current_subpath = Subpath(
+                        n_customers = data["n_customers"],
+                        starting_node = j,
+                        starting_time = current_subpath.current_time, 
+                        starting_charge = current_subpath.current_charge,
+                    )
+                elseif j in data["N_customers"]
+                    current_subpath.served[j] += 1
+                end
+                i = j
+            end
+            push!(
+                states, 
+                (current_subpath.starting_node, current_subpath.starting_time, current_subpath.starting_charge), 
+                (current_subpath.current_node, current_subpath.current_time, current_subpath.current_charge), 
+            )
+            add_subpath_to_generated_subpaths!(generated_subpaths, current_subpath)
+            for i in 1:(length(states)÷2)-1
+                add_charging_arc_to_generated_charging_arcs!(
+                    generated_charging_arcs, 
+                    ChargingArc(
+                        states[2*i][1],
+                        states[2*i][2],
+                        states[2*i][3],
+                        states[2*i+1][2] - states[2*i][2],
+                        states[2*i+1][2],
+                        states[2*i+1][3],
+                    )
+                )
+            end
+        end
+    end
+    return generated_subpaths, generated_charging_arcs
+end
+
 function subpath_formulation_column_generation_integrated_from_paths(
     G,
     data, 
     ;
+    method::String = "ours",
     path_single_service::Bool = false,
     subpath_single_service::Bool = false,
     path_check_customers::Bool = false,
@@ -792,6 +1180,7 @@ function subpath_formulation_column_generation_integrated_from_paths(
             # depots:                       %2d
             # charging stations:            %2d
             # vehicles:                     %2d
+            method:                         %s
             subpath_single_service:         %s
             subpath_check_customers:        %s
             path_single_service:            %s
@@ -802,6 +1191,7 @@ function subpath_formulation_column_generation_integrated_from_paths(
             data["n_depots"],
             data["n_charging"],
             data["n_vehicles"],
+            method,
             subpath_single_service,
             subpath_check_customers,
             path_single_service,
@@ -939,35 +1329,61 @@ function subpath_formulation_column_generation_integrated_from_paths(
         push!(params["ν"], mp_results["ν"])
         push!(params["lp_relaxation_solution_time_taken"], mp_results["solution_time_taken"])
 
-        base_labels_result = @timed generate_base_labels(
-            G, data, mp_results["κ"], mp_results["μ"], mp_results["ν"],
-            ;
-            single_service = subpath_single_service,
-            check_customers = subpath_check_customers,
-        )
-        base_labels_time = base_labels_result.time
-        full_labels_result = @timed find_nondominated_paths_notimewindows(
-            data, base_labels_result.value, mp_results["κ"], mp_results["μ"],
-            ;
-            single_service = path_single_service,
-            check_customers = path_check_customers,
-        )
-        full_labels_time = full_labels_result.time
-        (generated_subpaths, generated_charging_arcs) = get_subpaths_charging_arcs_from_negative_paths(
-            data, full_labels_result.value,
-        )
-        push!(
-            params["sp_base_time_taken"],
-            round(base_labels_time, digits=3)
-        )
-        push!(
-            params["sp_full_time_taken"],
-            round(full_labels_time, digits=3)
-        )
-        push!(
-            params["sp_total_time_taken"],
-            round(base_labels_time + full_labels_time, digits=3)
-        )
+        if method == "ours"
+            base_labels_result = @timed generate_base_labels(
+                G, data, mp_results["κ"], mp_results["μ"], mp_results["ν"],
+                ;
+                single_service = subpath_single_service,
+                check_customers = subpath_check_customers,
+            )
+            base_labels_time = base_labels_result.time
+            full_labels_result = @timed find_nondominated_paths_notimewindows(
+                data, base_labels_result.value, mp_results["κ"], mp_results["μ"],
+                ;
+                single_service = path_single_service,
+                check_customers = path_check_customers,
+            )
+            full_labels_time = full_labels_result.time
+            (generated_subpaths, generated_charging_arcs) = get_subpaths_charging_arcs_from_negative_paths(
+                data, full_labels_result.value,
+            )
+            push!(
+                params["sp_base_time_taken"],
+                round(base_labels_time, digits=3)
+            )
+            push!(
+                params["sp_full_time_taken"],
+                round(full_labels_time, digits=3)
+            )
+            push!(
+                params["sp_total_time_taken"],
+                round(base_labels_time + full_labels_time, digits=3)
+            )
+        elseif method == "benchmark"
+            full_labels_result = @timed find_nondominated_paths(
+                G, data, mp_results["κ"], mp_results["μ"], mp_results["ν"],
+                ;
+                time_windows = false,
+                single_service = path_single_service,
+                check_customers = path_check_customers,
+            )
+            full_labels_time = full_labels_result.time
+            (generated_subpaths, generated_charging_arcs) = get_subpaths_charging_arcs_from_negative_path_labels(
+                data, full_labels_result.value,
+            )
+            push!(
+                params["sp_base_time_taken"],
+                0.0
+            )
+            push!(
+                params["sp_full_time_taken"],
+                round(full_labels_time, digits=3)
+            )
+            push!(
+                params["sp_total_time_taken"],
+                round(full_labels_time, digits=3)
+            )
+        end
 
         if length(generated_subpaths) == 0
             push!(params["number_of_new_subpaths"], 0)
