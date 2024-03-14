444fc5f8395963b1c235590fc2e21d9b40cce2c4
Sean Lo <seanlo@mit.edu>
2024-03-13 20:12:38 -0400

    remove unnecessary tests, change script

--- experiments/ours_benchmark/01/script.jl
+++ experiments/ours_benchmark/01/script.jl
@@ -106,73 +106,81 @@ function run_instance(
         some_paths, model, z, SR3_constraints
     ) = run.value;
 
-    some_paths_metrics = compute_path_metrics(some_paths)
-    
-    records = [
-        (
-            n_depots = n_depots,
-            n_customers = n_customers,
-            n_charging = n_charging,
-            n_vehicles = n_vehicles,
-            depot_pattern = depot_pattern,    
-            customer_pattern = customer_pattern,
-            charging_pattern = charging_pattern,
-            customer_spread = customer_spread,
-            xmin = xmin,
-            xmax = xmax,
-            ymin = ymin,
-            ymax = ymax,
-            T = T,
-            seed = seed,
-            B = B,
-            μ = μ,
-            travel_cost_coeff = travel_cost_coeff,
-            charge_cost_coeff = charge_cost_coeff,
-            load_scale = load_scale,
-            load_shape = load_shape,
-            load_tolerance = load_tolerance,
-            batch = batch,
-            permissiveness = permissiveness,
-            use_load = use_load,
-            use_time_windows = use_time_windows,
-            method = method,
-            elementary = elementary,
-            ngroute = ngroute,
-            ngroute_neighborhood_size = Int(ceil(sqrt(graph.n_customers))),
-            ngroute_neighborhood_depots_size = "small",
-            ngroute_neighborhood_charging_size = "small",
-            use_smaller_graph = false,
-            use_adaptive_ngroute = false,
-            use_SR3_cuts = false,
-            use_lmSR3_cuts = false,
-            # Time taken
-            time_taken = CG_all_params[1]["time_taken"],
-            sp_base_time_taken_total = CG_all_params[1]["sp_base_time_taken_total"],
-            sp_full_time_taken_total = CG_all_params[1]["sp_full_time_taken_total"],
-            sp_time_taken_total = CG_all_params[1]["sp_time_taken_total"],
-            lp_relaxation_time_taken_total = CG_all_params[1]["lp_relaxation_time_taken_total"],
-            sp_base_time_taken_mean = CG_all_params[1]["sp_base_time_taken_mean"],
-            sp_full_time_taken_mean = CG_all_params[1]["sp_full_time_taken_mean"],
-            sp_time_taken_mean = CG_all_params[1]["sp_time_taken_mean"],
-            lp_relaxation_time_taken_mean = CG_all_params[1]["lp_relaxation_time_taken_mean"],
-            # n_iterations
-            counter = CG_all_params[1]["counter"],
-            converged = CG_all_params[1]["converged"],
-            time_limit_reached = CG_all_params[1]["time_limit_reached"],
-            # Objective values and gaps
-            LP_infeasible = all_params[1]["CGLP_infeasible"],
-            LP_objective = all_params[1]["CGLP_objective"],
-            IP_infeasible = all_params[1]["CGIP_infeasible"],
-            IP_objective = all_params[1]["CGIP_objective"],
-            LP_IP_gap = all_params[1]["CG_LP_IP_gap"],
-            # Length metrics 
-            mean_subpath_length = get(some_paths_metrics, "mean_subpath_length", missing),
-            mean_path_length = get(some_paths_metrics, "mean_path_length", missing),
-            mean_ps_length = get(some_paths_metrics, "mean_ps_length", missing),
-        )
-    ]
+    local records
     if write_log
+        if length(all_params) == 0
+            return
+        end
+        if all_params[end]["errored"]
+            println("Row index $(row_index) encountered errored model.")
+            return
+        end
+        some_paths_metrics = compute_path_metrics(some_paths)
+        records = [
+            (
+                n_depots = n_depots,
+                n_customers = n_customers,
+                n_charging = n_charging,
+                n_vehicles = n_vehicles,
+                depot_pattern = depot_pattern,    
+                customer_pattern = customer_pattern,
+                charging_pattern = charging_pattern,
+                customer_spread = customer_spread,
+                xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax,
+                T = T,
+                seed = seed,
+                B = B,
+                μ = μ,
+                travel_cost_coeff = travel_cost_coeff,
+                charge_cost_coeff = charge_cost_coeff,
+                load_scale = load_scale,
+                load_shape = load_shape,
+                load_tolerance = load_tolerance,
+                batch = batch,
+                permissiveness = permissiveness,
+                use_load = use_load,
+                use_time_windows = use_time_windows,
+                method = method,
+                elementary = elementary,
+                ngroute = ngroute,
+                ngroute_neighborhood_size = Int(ceil(sqrt(graph.n_customers))),
+                ngroute_neighborhood_depots_size = "small",
+                ngroute_neighborhood_charging_size = "small",
+                use_smaller_graph = false,
+                use_adaptive_ngroute = false,
+                use_SR3_cuts = false,
+                use_lmSR3_cuts = false,
+                # Time taken
+                time_taken = CG_all_params[end]["time_taken"],
+                sp_base_time_taken_total = CG_all_params[end]["sp_base_time_taken_total"],
+                sp_full_time_taken_total = CG_all_params[end]["sp_full_time_taken_total"],
+                sp_time_taken_total = CG_all_params[end]["sp_time_taken_total"],
+                lp_relaxation_time_taken_total = CG_all_params[end]["lp_relaxation_time_taken_total"],
+                sp_base_time_taken_mean = CG_all_params[end]["sp_base_time_taken_mean"],
+                sp_full_time_taken_mean = CG_all_params[end]["sp_full_time_taken_mean"],
+                sp_time_taken_mean = CG_all_params[end]["sp_time_taken_mean"],
+                lp_relaxation_time_taken_mean = CG_all_params[end]["lp_relaxation_time_taken_mean"],
+                # n_iterations
+                counter = CG_all_params[end]["counter"],
+                converged = CG_all_params[end]["converged"],
+                time_limit_reached = CG_all_params[end]["time_limit_reached"],
+                # Objective values and gaps
+                LP_artificial = all_params[end]["CGLP_artificial"],
+                LP_objective = all_params[end]["CGLP_objective"],
+                IP_artificial = all_params[end]["CGIP_artificial"],
+                IP_objective = all_params[end]["CGIP_objective"],
+                LP_IP_gap = all_params[end]["CG_LP_IP_gap"],
+                # Length metrics 
+                mean_subpath_length = get(some_paths_metrics, "mean_subpath_length", missing),
+                mean_path_length = get(some_paths_metrics, "mean_path_length", missing),
+                mean_ps_length = get(some_paths_metrics, "mean_ps_length", missing),
+            )
+        ]
         CSV.write("$(@__DIR__)/records/$(row_index).csv", DataFrame(records))
+        println("$records")
     end
     if write_results
         mkpath("$(@__DIR__)/results/$(row_index)")
@@ -180,7 +188,8 @@ function run_instance(
         savefig(plot_path_solution(CGLP_all_results[end], data, graph, some_paths), "$(@__DIR__)/results/$(row_index)/plot_path_solution_LP.png")
         savefig(plot_path_solution(CGIP_all_results[end], data, graph, some_paths), "$(@__DIR__)/results/$(row_index)/plot_path_solution_IP.png")
     end
-    println("Row $row_index processed: $(args_df[row_index, :])")
+    println("Row $row_index processed. ") 
+    println()
 end
 
 
@@ -207,12 +216,7 @@ n_tasks = parse(Int, ARGS[2])
 println("Processing rows: $(collect(task_index:n_tasks:size(args_df, 1)))")
 
 for row_index in task_index:n_tasks:size(args_df, 1)
-    run_instance(args_df, row_index, 3600.0, write_log = true)
-end
-
-begin
-    tune_args_df = DataFrame(CSV.File("$(@__DIR__)/tune_args.csv"))
-    for i in 1:nrow(tune_args_df)
-        run_instance(tune_args_df, i, 100.0, write_log = true)
+    if !isfile("$(@__DIR__)/records/$row_index.csv")
+        run_instance(args_df, row_index, 3600.0, write_log = true)
     end
 end
