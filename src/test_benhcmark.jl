95a0f20fd16aeecddad829a70344cdf341a36d28
Sean Lo <seanlo@mit.edu>
2024-06-11 16:38:14 -0400

    test files (unstable)

--- src/test_benhcmark.jl
+++ src/test_benhcmark.jl
@@ -1,5 +1,3 @@
-include("arc_formulation.jl")
-include("subpath_formulation.jl")
 include("path_formulation.jl")
 include("utils.jl")
 
@@ -7,11 +5,12 @@ using CSV, DataFrames
 using BenchmarkTools
 using Test
 
-args_df = CSV.read("$(@__DIR__)/../experiments/adaptive_ngroute_lmsr3/01a/args.csv", DataFrame)
-row_index = 3
+args_df = CSV.read("$(@__DIR__)/../experiments/ours_benchmark/01a/args.csv", DataFrame)
+row_index = 5760
 
 n_depots = args_df[row_index, :n_depots]
 n_customers = args_df[row_index, :n_customers]
+# n_customers = 20
 n_charging = args_df[row_index, :n_charging]
 depot_pattern = String(args_df[row_index, :depot_pattern])
 customer_pattern = String(args_df[row_index, :customer_pattern])
@@ -27,8 +26,8 @@ B = args_df[row_index, :B]
 μ = args_df[row_index, :μ]
 seed = args_df[row_index, :seed]
 travel_cost_coeff = args_df[row_index, :travel_cost_coeff]
-# charge_cost_coeff = args_df[row_index, :charge_cost_coeff]
-charge_cost_coeff = 3
+charge_cost_coeff = args_df[row_index, :charge_cost_coeff]
+# charge_cost_coeff = 3
 load_scale = args_df[row_index, :load_scale]
 load_shape = args_df[row_index, :load_shape]
 load_tolerance = args_df[row_index, :load_tolerance]
@@ -38,11 +37,8 @@ permissiveness = args_df[row_index, :permissiveness]
 use_load = args_df[row_index, :use_load]
 use_time_windows = args_df[row_index, :use_time_windows]
 
-method = String(args_df[row_index, :method])
-ngroute_neighborhood_charging_size = String(args_df[row_index, :ngroute_neighborhood_charging_size])
 use_lmSR3_cuts = args_df[row_index, :use_lmSR3_cuts]
 max_SR3_cuts = args_df[row_index, :max_SR3_cuts]
-
 data = generate_instance(
     n_depots = n_depots,
     n_customers = n_customers,
@@ -71,89 +67,39 @@ data = generate_instance(
     data_dir = "../../../data/",
 )
 graph = generate_graph_from_data(data)
+plot_instance(data, plot_edges = true, graph = graph)
 
-@time SR3_results = path_formulation_column_generation_with_adaptve_ngroute_SR3_cuts(
+using Profile
+
+@timev ours_results = path_formulation_column_generation_with_adaptve_ngroute_SR3_cuts(
     data, graph,
     ;
-    method = method,
+    method = "ours",
     elementary = false,
     ngroute = true,
-    ngroute_neighborhood_size = Int(ceil(sqrt(graph.n_customers))),
+    ngroute_neighborhood_size = Int(ceil(graph.n_customers)),
     ngroute_neighborhood_depots_size = "small", 
-    ngroute_neighborhood_charging_size = ngroute_neighborhood_charging_size, 
+    ngroute_neighborhood_charging_size = "small",
     verbose = true,
     use_adaptive_ngroute = true,
     use_SR3_cuts = true,
     use_lmSR3_cuts = false,
+    max_SR3_cuts = 5,
+    time_limit = 60.0,
 );
 
-@time lmSR3_results = path_formulation_column_generation_with_adaptve_ngroute_SR3_cuts(
+@timev benchmark_results = path_formulation_column_generation_with_adaptve_ngroute_SR3_cuts(
     data, graph,
     ;
-    method = method,
+    method = "benchmark",
     elementary = false,
     ngroute = true,
-    ngroute_neighborhood_size = Int(ceil(sqrt(graph.n_customers))),
+    ngroute_neighborhood_size = Int(ceil(graph.n_customers)),
     ngroute_neighborhood_depots_size = "small", 
-    ngroute_neighborhood_charging_size = ngroute_neighborhood_charging_size, 
+    ngroute_neighborhood_charging_size = "small",
     verbose = true,
     use_adaptive_ngroute = true,
     use_SR3_cuts = true,
-    use_lmSR3_cuts = true,
+    use_lmSR3_cuts = false,
+    max_SR3_cuts = 5,
 );
-
-using Plots
-Plots.scatter(
-    cumsum([x["implemented_SR3_cuts_count"] for x in lmSR3_results[5]]),
-    [x["CG_sp_time_taken_mean"] for x in lmSR3_results[5]],
-    xlabel = "Number of cuts",
-    ylabel = "Pricing problem time taken (s)",
-)
-Plots.scatter(
-    cumsum([x["implemented_SR3_cuts_count"] for x in SR3_results[5]]),
-    [x["CG_sp_time_taken_mean"] for x in SR3_results[5]],
-    xlabel = "Number of cuts",
-    ylabel = "Pricing problem time taken (s)",
-)
-
-
-(method, use_lmSR3_cuts) = ("ours", false)
-Env = nothing
-time_windows = false
-verbose = true
-ngroute_neighborhood_size = Int(ceil(sqrt(graph.n_customers)))
-ngroute_neighborhood_depots_size = "small"
-ngroute_neighborhood_charging_size = "small"
-
-SR3_constraints = Dict{
-    Tuple{NTuple{3, Int}, Tuple{Vararg{Int}}},
-    ConstraintRef,
-}()
-SR3_list = Tuple{Float64, NTuple{3, Int}, Tuple{Vararg{Int}}}[]
-
-
-
-neighborhoods = compute_ngroute_neighborhoods(
-    graph,
-    ngroute_neighborhood_size; 
-    depots_size = ngroute_neighborhood_depots_size,
-    charging_size = ngroute_neighborhood_charging_size,
-)
-
-artificial_paths = generate_artificial_paths(data, graph)
-some_paths = deepcopy(artificial_paths)
-path_costs = compute_path_costs(
-    data, graph, 
-    some_paths,
-)
-path_service = compute_path_service(
-    graph,
-    some_paths,
-)
-printlist = String[]
-
-model, z = path_formulation_build_model(
-    data, graph, some_paths, path_costs, path_service,
-    ; 
-    Env = Env,
-)
--- src/test_sparse_graph.jl
+++ src/test_sparse_graph.jl
@@ -1,41 +1,77 @@
 include("path_formulation.jl")
 include("utils.jl")
 
-using BenchmarkTools, Test
+using CSV, DataFrames
+using BenchmarkTools
+using Test
 
+args_df = CSV.read("$(@__DIR__)/../experiments/scalability/01a/args.csv", DataFrame)
+row_index = 1
+
+n_depots = args_df[row_index, :n_depots]
+n_customers = args_df[row_index, :n_customers]
+n_charging = args_df[row_index, :n_charging]
+depot_pattern = String(args_df[row_index, :depot_pattern])
+customer_pattern = String(args_df[row_index, :customer_pattern])
+charging_pattern = String(args_df[row_index, :charging_pattern])
+customer_spread = args_df[row_index, :customer_spread]
+xmin = args_df[row_index, :xmin]
+xmax = args_df[row_index, :xmax]
+ymin = args_df[row_index, :ymin]
+ymax = args_df[row_index, :ymax]
+n_vehicles = args_df[row_index, :n_vehicles]
+T = args_df[row_index, :T]
+B = args_df[row_index, :B]
+μ = args_df[row_index, :μ]
+seed = args_df[row_index, :seed]
+travel_cost_coeff = args_df[row_index, :travel_cost_coeff]
+charge_cost_coeff = args_df[row_index, :charge_cost_coeff]
+# charge_cost_coeff = 3
+load_scale = args_df[row_index, :load_scale]
+load_shape = args_df[row_index, :load_shape]
+load_tolerance = args_df[row_index, :load_tolerance]
+batch = args_df[row_index, :batch]
+permissiveness = args_df[row_index, :permissiveness]
+
+use_load = args_df[row_index, :use_load]
+use_time_windows = args_df[row_index, :use_time_windows]
+sparse_graph = args_df[row_index, :sparse_graph]
+
+method = String(args_df[row_index, :method])
+use_lmSR3_cuts = args_df[row_index, :use_lmSR3_cuts]
+max_SR3_cuts = args_df[row_index, :max_SR3_cuts]
 data = generate_instance(
-    n_depots = 4,
-    n_customers = 40,
-    n_charging = 21,
-    n_vehicles = 8,
-    depot_pattern = "grid",
-    customer_pattern = "random_box",
-    charging_pattern = "grid_clipped",
-    customer_spread = 1e-3,
-    xmin = 0.0,
-    xmax = 4.0,
-    ymin = 0.0,
-    ymax = 4.0,
-    T = 100000,
-    seed = 1,
-    B = 15000,
-    μ = 5,
-    travel_cost_coeff = 7,
-    charge_cost_coeff = 3,
-    load_scale = 5.0,
-    load_shape = 20.0,
-    load_tolerance = 1.3,
-    batch = 1,
-    permissiveness = 0.2,
+    n_depots = n_depots,
+    n_customers = n_customers,
+    n_charging = n_charging,
+    n_vehicles = n_vehicles,
+    depot_pattern = depot_pattern,
+    customer_pattern = customer_pattern,
+    charging_pattern = charging_pattern,
+    customer_spread = customer_spread,
+    xmin = xmin,
+    xmax = xmax,
+    ymin = ymin,
+    ymax = ymax,
+    T = T,
+    seed = seed,
+    B = B,
+    μ = μ,
+    travel_cost_coeff = travel_cost_coeff,
+    charge_cost_coeff = charge_cost_coeff,
+    load_scale = load_scale,
+    load_shape = load_shape,
+    load_tolerance = load_tolerance,
+    batch = batch,
+    permissiveness = permissiveness,
     ;
-    # data_dir = "../../../data/",
+    data_dir = "../../../data/",
 )
-graph = generate_graph_from_data(data, sparse = true, sparse_linear_max_q = 2.0 * data.B)
+graph = generate_graph_from_data(data, sparse = true)
 plot_instance(data, plot_edges = true, graph = graph)
 length(edges(graph.G))
 
-graph = generate_graph_from_data(data, sparse = false)
-length(edges(graph.G))
+sparse_graph
 
 using Profile
 
